e566f254404073da3a609e2231426407a517589e
Raigis <alerish015@gmail.com>
2023-10-26 16:27:12 +0300

    Start

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Это попытка создать простой консольный рогалик
+void Start () {
+    string answer;
+    Console.Write($"Добро пожаловать!\nЕсли готовы начать, введите 'Y'.\nЕсли желаете выйти, нажмите 'N'.\n[Y/N]\n");
+    while(true){
+        answer = Console.ReadLine();
+        if(answer.ToLower() != "y" && answer.ToLower() != "n") {
+            Console.Write($"Неизвестный ввод.\nВведите 'Y' или 'N'.\n[Y/N]\n");
+        } else break;
+    }
+    if (answer.ToLower() == "y"){
+        Console.WriteLine($"Мы начинаем!");
+        Console.WriteLine($"Заглушка");
+    } else {
+        Console.WriteLine($"До свидания!");
+    }
+}
+
+Start();
