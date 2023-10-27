b1e232c005b52fdfada73429f2940edee341a15f
Raigis <alerish015@gmail.com>
2023-10-27 12:05:35 +0300

    Status

--- Program.cs
+++ Program.cs
@@ -1,5 +1,48 @@
-﻿// Это попытка создать простой консольный рогалик
-using System.Collections;
+﻿using System.Collections;
+int CountRoom(int counter = 0){
+    int count;
+    if (counter == 0) {
+        count = 0;
+    } else count = 0 + counter;
+    return count;
+}
+
+int Changer(string parametr, int chHP = 0, int chDEF = 0, int chATK = 0){
+    int changeHP;
+    int changeDEF;
+    int changeATK;
+    if (CountRoom() == 0) {
+        changeHP = chHP;
+        changeDEF = chDEF;
+        changeATK = chATK;
+    } else {
+        changeHP = 0 + chHP;
+        changeDEF = 0 + chDEF;
+        changeATK = 0 + chATK;
+    }
+    switch (parametr){
+        case "HP": return changeHP;
+        case "DEF": return changeDEF;
+        case "ATK": return changeATK;
+        default: return -1;
+    }
+}
+
+int Status (string parametr) {
+    int hp = 100;
+    int def = 0;
+    int atk = 5;
+    switch (parametr){
+        case "HPFR": return hp;
+        case "HP": return hp + Changer(parametr);
+        case "DEFFR": return def;
+        case "DEF": return def + Changer(parametr);
+        case "ATKFR": return atk;
+        case "ATK": return atk + Changer(parametr);
+        default: return -1;
+    }
+}
+
 void Switcher (string answer){
     switch(answer){
     case "1": 
@@ -29,9 +72,9 @@ void FirstRoom () {
     Console.WriteLine($"<__   __>");
     Console.WriteLine($"|__|*|__|");
     Console.WriteLine($"\nЭто первая комната.\nСтрелками обозначены пути, куда ты можешь пойти.\nЗвёздочка же указывает на твоё место положение.\nДля выбора направления введи число, соответствующее нужной стрелке в списке внизу.\nТакже, левее списка выбора можно увидеть свои характеристики.\nЕсли желаете выйти, введите 'EXIT'.\n");
-    Console.WriteLine($"1)^\t\t\t\t\t\tHP:100");
-    Console.WriteLine($"2)<\t\t\t\t\t\tDEF:100");
-    Console.WriteLine($"3)>\t\t\t\t\t\tATK:5");
+    Console.WriteLine($"1)^\t\t\t\t\t\tHP:{Status("HPFR")}");
+    Console.WriteLine($"2)<\t\t\t\t\t\tDEF:{Status("DEFFR")}");
+    Console.WriteLine($"3)>\t\t\t\t\t\tATK:{Status("ATKFR")}");
     Console.WriteLine($"\n[1/2/3/EXIT]");
     while(true){
         answer = Console.ReadLine();
